3f65edbd967db9e82597357e957d64f2705c8855
daianaadepaula <daianaadepaula1@gmail.com>
2025-06-08 17:59:10 -0300

    feat: chance port

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ import { connectDB } from "./lib/db.js"
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 10000
 
 app.use(cors())
 app.use(express.json())
